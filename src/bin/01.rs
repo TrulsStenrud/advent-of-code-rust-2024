fc3aaaf309bb400bb54031c2aa2529a653473bf8
Truls <truls.stenrud@gmail.com>
2024-12-02 11:15:03 +0100

    Add brute force fallback

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,9 @@
 advent_of_code::solution!(1);
 
-fn count_occurrances(lsit: &Vec<u32>, value: &u32) -> u32 {
+fn count_occurrances(list: &Vec<u32>, value: &u32) -> u32 {
     let mut count = 0;
-    for n in lsit {
-        if n == value {
+    for value in list {
+        if value == value {
             count += 1;
         }
     }
---
+++ src/bin/02.rs
@@ -0,0 +1,134 @@
+advent_of_code::solution!(2);
+
+fn is_safe(line: &str) -> bool {
+    let mut numbers = line.split_whitespace();
+    let mut prev = numbers
+        .next()
+        .expect("Not enough numbers in line.")
+        .parse::<u32>()
+        .expect("Failed to parse u32 from string.");
+
+    let second = numbers
+        .next()
+        .expect("Not enough numbers in line.")
+        .parse::<u32>()
+        .expect("Failed to parse u32 from string.");
+
+    let increasing = prev < second;
+    if prev == second || prev.abs_diff(second) > 3 {
+        return false;
+    }
+    prev = second;
+    for curr in numbers {
+        let next = curr
+            .parse::<u32>()
+            .expect("Failed to parse u32 from string.");
+        // println!("{} {} {}", next, prev, prev == next);
+        if (prev == next) || ((prev < next) != increasing) || prev.abs_diff(next) > 3 {
+            // println!("{} was false", line);
+            return false;
+        }
+        prev = next;
+    }
+    // println!("{} was true", line);
+    true
+}
+fn is_safe_ish(line: &str) -> bool {
+    let mut numbers = line.split_whitespace();
+    let mut prev = numbers
+        .next()
+        .expect("Not enough numbers in line.")
+        .parse::<u32>()
+        .expect("Failed to parse u32 from string.");
+
+    let second = numbers
+        .next()
+        .expect("Not enough numbers in line.")
+        .parse::<u32>()
+        .expect("Failed to parse u32 from string.");
+
+    let mut has_false = false;
+
+    let increasing = prev < second;
+    if prev == second || prev.abs_diff(second) > 3 {
+        has_false = true;
+    } else {
+        prev = second;
+    }
+    for curr in numbers {
+        let next = curr
+            .parse::<u32>()
+            .expect("Failed to parse u32 from string.");
+        // println!("{} {} {}", next, prev, prev == next);
+        if (prev == next) || ((prev < next) != increasing) || prev.abs_diff(next) > 3 {
+            if has_false {
+                // println!("{} was false", line);
+                return false;
+            }
+            has_false = true;
+        } else {
+            prev = next;
+        }
+    }
+    // println!("{} was true", line);
+    true
+}
+
+fn are_you_sure(line: &str) -> bool {
+    let numbers = line
+        .split_whitespace()
+        .map(|n| n.parse::<u32>().expect("Unable to parse u32"))
+        .collect::<Vec<_>>();
+
+    for i in 0..numbers.len() {
+        let mut new_numbers = numbers.clone();
+        new_numbers.remove(i);
+        if is_safe(
+            &new_numbers
+                .iter()
+                .map(|n| n.to_string())
+                .collect::<Vec<_>>()
+                .join(" "),
+        ) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(input.lines().filter(|line| is_safe(line)).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .filter(|line| {
+                let result = is_safe_ish(line);
+                if result {
+                    return result;
+                } else {
+                    return are_you_sure(line);
+                }
+            })
+            .count() as u32,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_tw1() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4));
+    }
+}
