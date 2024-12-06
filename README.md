673c08a2525d7b35315ec03968472836910339f1
Truls <truls.stenrud@gmail.com>
2024-12-06 14:51:15 +0100

    Speed

--- README.md
+++ README.md
@@ -26,8 +26,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 3](./src/bin/03.rs) | `138.6µs` | `188.5µs` |
 | [Day 4](./src/bin/04.rs) | `411.0µs` | `157.0µs` |
 | [Day 5](./src/bin/05.rs) | `323.4µs` | `303.7µs` |
+| [Day 6](./src/bin/06.rs) | `260.2µs` | `-` |
 
-**Total: 1.80ms**
+**Total: 2.07ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -5,12 +5,18 @@ advent_of_code::solution!(6);
 pub fn part_one(input: &str) -> Option<u32> {
     let mut pos: (isize, isize) = (0, 0);
     let mut direction: (isize, isize) = (1, 1);
-    let mut trees = HashSet::new();
-    let mut max_x: isize = 0;
-    let mut max_y: isize = 0;
+    let max_x: isize = input.lines().count() as isize - 1;
+    let max_y: isize = input.lines().next().unwrap().len() as isize - 1;
+
+    let mut trees = HashSet::with_capacity((max_x * max_y) as usize);
+
     input.lines().enumerate().for_each(|(x, line)| {
         line.chars().enumerate().for_each(|(y, char)| {
-            if char == '#' {
+            // max_x = max(max_x, x as isize);
+            // max_y = max(max_y, y as isize);
+            if char == '.' {
+                // Do nothing
+            } else if char == '#' {
                 trees.insert((x as isize, y as isize));
             } else if char == '<' {
                 pos = (x as isize, y as isize);
@@ -25,12 +31,10 @@ pub fn part_one(input: &str) -> Option<u32> {
                 pos = (x as isize, y as isize);
                 direction = (1, 0);
             }
-            max_x = max(max_x, x as isize);
-            max_y = max(max_y, y as isize);
         });
     });
 
-    let mut walked_locations = HashSet::new();
+    let mut walked_locations = HashSet::with_capacity((max_x * max_y) as usize);
 
     let mut next = (pos.0 + direction.0, pos.1 + direction.1);
     while pos.0 > 0 && pos.0 <= max_x && pos.1 > 0 && pos.1 <= max_y {
